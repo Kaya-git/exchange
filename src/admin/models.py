7b0272b6389212b92d125c05596286effa4be269
Kaya <ezjell@yandex.ru>
2023-10-24 22:32:01 +0300

    refac

--- src/admin/models.py
+++ src/admin/models.py
@@ -13,11 +13,14 @@ from typing import Optional
 
 
 class AdminAuth(AuthenticationBackend):
-    async def login(
-        self,
-        request: Request,
-    ) -> bool:
+    async def login(self, request: Request) -> bool:
+        form = await request.form()
+        username, password = form["username"], form["password"]
+
+        # Validate username/password credentials
+        # And update session
         request.session.update({"token": "..."})
+
         return True
 
     async def logout(self, request: Request) -> bool:
