cf09a9ded16faec921cb59ea2b84b12454449c57
Kaya <ezjell@yandex.ru>
2023-11-06 12:09:17 +0300

    faq and contacts

--- src/contacts/routers.py
+++ src/contacts/routers.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Path
 import sqlalchemy as sa
 from database.db import Database, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
+from .schemas import Contact
+from typing import List, Annotated
 
 
 contact_router = APIRouter(
@@ -12,16 +14,16 @@ contact_router = APIRouter(
 @contact_router.get("/")
 async def contact_list(
     async_session: AsyncSession = Depends(get_async_session)
-):
+) -> List[Contact]:
     db = Database(session=async_session)
     contact_list = await db.contact.get_all()
     return contact_list
 
 @contact_router.get("/{id}")
 async def contact(
-    id: int,
+    id: Annotated[int, Path(title="The ID of the item to get")],
     async_session: AsyncSession = Depends(get_async_session)
-):
+) -> Contact:
     db = Database(session=async_session)
     contact = await db.contact.get(ident=id)
     return contact
