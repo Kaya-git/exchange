6e99dfaa35e287f3a322a891a22c3a1f2f3467f2
Kaya <ezjell@yandex.ru>
2023-12-14 13:01:18 +0300

    allow headers explicit

--- back/src/main.py
+++ back/src/main.py
@@ -56,7 +56,8 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
-    allow_headers=["*"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization"],
 )
 
 
